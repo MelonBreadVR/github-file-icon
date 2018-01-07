6d1fced4dae432912ebfd5f6d8b1e05679c30d79
xxhomey19 <xxhomey19@gmail.com>
2018-01-07 10:40:22 +0800

    Delay for 500 ms

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,24 +1,27 @@
+import delay from 'delay';
 import fileIcons from 'file-icons-js';
 
 import '../css/icons.css';
 
-const filenameDoms = Array.from(
-  document.querySelectorAll('tr.js-navigation-item > td.content > span > a')
-);
-const iconDoms = Array.from(
-  document.querySelectorAll('tr.js-navigation-item > td.icon')
-);
+delay(500).then(() => {
+  const filenameDoms = Array.from(
+    document.querySelectorAll('tr.js-navigation-item > td.content > span > a')
+  );
+  const iconDoms = Array.from(
+    document.querySelectorAll('tr.js-navigation-item > td.icon')
+  );
 
-const filenameDomsLength = filenameDoms.length;
+  const filenameDomsLength = filenameDoms.length;
 
-if (filenameDomsLength !== 0) {
-  for (let i = 0; i < filenameDomsLength; i += 1) {
-    const { innerText: filename } = filenameDoms[i];
-    const iconDom = iconDoms[i];
-    const className = fileIcons.getClassWithColor(filename);
+  if (filenameDomsLength !== 0) {
+    for (let i = 0; i < filenameDomsLength; i += 1) {
+      const { innerText: filename } = filenameDoms[i];
+      const iconDom = iconDoms[i];
+      const className = fileIcons.getClassWithColor(filename);
 
-    if (className) {
-      iconDom.innerHTML = `<span class='${className}'></span>`;
+      if (className) {
+        iconDom.innerHTML = `<span class='${className}'></span>`;
+      }
     }
   }
-}
+});
